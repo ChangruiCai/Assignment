87109959c2e6b8fc9a1342f8d2edccbdcc77120e
ChangruiCai <changrui05@ucla.edu>
2024-11-29 20:44:33 -0800

    Update Problem3.m

--- HW1/Problem3/Problem3.m
+++ HW1/Problem3/Problem3.m
@@ -1,1 +1,401 @@
+clear; clc; close all;
+
+% Number of nodes
+N = 50;
+ndof = N * 2; % # of DoF
+
+dt = 0.01; % second - Time step size
+
+% Density
+rho_metal = 2700; % kg/m^3
+
+r = 0.011;
+R = 0.013;
+Y = 70*1e9; % Young's modulus (Y instead of E for clarity)
+
+g = 9.8; % m/s^2 - gravity
+
+totalTime = 1; % seconds
+
+RodLength = 1; % meter
+deltaL = RodLength / (N-1);
+
+% Utility quantities
+ne = N - 1; % Number of edges
+EI = Y * pi * (R^4-r^4) / 4; % Nm^2 - bending stiffness
+EA = Y * pi * (R^2-r^2); % Newton
+
+% Mass matrix
+dm = pi * (R^2-r^2) * RodLength * rho_metal / (N - 1);
+M = eye(ndof) * dm;
+
+% tolerance
+tol = EI / RodLength^2 * 1e-3;
+
+% Number of time steps y-velocity of the middle node
+Nsteps = round( totalTime / dt ) + 1;
+
+% Fixed and Free DoFs
+fixedDOF = [1; 2; ndof];
+freeDOF = 3:ndof-1;
+boundaryConditionVector = [0; 0; 0];
+
+% "Gravity" - P External Force
+PP0 = 2e3:1e3:2e4;
+YY_max = zeros(1,length(PP0));
+yy_max = zeros(1,length(PP0));
+
+for i = 1:1
+% Geometry - initial configuration
+nodes = zeros(N, 2); % Loop over all the nodes
+for c = 1:N
+    nodes(c,1) = (c-1) * deltaL; % x coordinates
+    
+end
+
+Point = 0.75;
+[~,ind_P] = min(abs(nodes(:,1) - Point));
+P0 = PP0(i); 
+P = zeros(ndof,1);
+P(2*ind_P) = -P0;
+
+% Initial DOF
+q0 = zeros(ndof,1); % loop over nodes
+for c=1:N
+    q0 ( 2*c - 1 ) = nodes(c,1); % x1, x2, x3
+    q0 ( 2*c ) = nodes(c,2); % y1, y2, y3
+end
+
+u0 = zeros(ndof,1); % old velocity (initial velocity)
+
+% Store information
+Q = zeros(ndof, Nsteps);
+Q(:,1) = q0;
+Y_max = zeros(1, Nsteps);
+
+% Time marching scheme
+
+for c=2:Nsteps
+     
+    q = q0; % Guess
+    q(fixedDOF) = boundaryConditionVector;
+    
+    % Newton Raphson
+    err = 10 * tol;
+    while err > tol
+
+        q_free = q(freeDOF);
+
+        % Inertia
+        f = M / dt * ( (q-q0) / dt - u0 );
+        J = M / dt^2;
+        %% Elastic forces
+        % Stretching energy between nodes k and k+1
+        for k = 1:N-1
+            xk = q(2*k-1);
+            yk = q(2*k);
+            xkp1 = q(2*k+1);
+            ykp1 = q(2*k+2);
+            dF = gradEs(xk, yk, xkp1, ykp1, deltaL, EA);
+            dJ = hessEs(xk, yk, xkp1, ykp1, deltaL, EA);
+            ind = 2*k-1:2*k+2;
+            f(ind) = f(ind) + dF;
+            J(ind,ind) = J(ind,ind) + dJ;
+        end
+        
+        .
+        for k = 2:N-1
+            xkm1 = q(2*k-3);
+            ykm1 = q(2*k-2);
+            xk = q(2*k-1);
+            yk = q(2*k);
+            xkp1 = q(2*k+1);
+            ykp1 = q(2*k+2);
+            curvature0 = 0;
+            dF = gradEb(xkm1, ykm1, xk, yk, xkp1, ykp1, curvature0, deltaL, EI);
+            dJ = hessEb(xkm1, ykm1, xk, yk, xkp1, ykp1, curvature0, deltaL, EI);
+            ind = 2*k-3:2*k+2;
+            f(ind) = f(ind) + dF;
+            J(ind,ind) = J(ind,ind) + dJ;
+        end
+        
+        % Weight
+        f = f - P;
+        
+        
+        f_free = f(freeDOF);
+        J_free = J(freeDOF,freeDOF);
+        
+        % Update
+        dq_free = J_free \ f_free;
+        q_free = q_free - dq_free;
+
+        err = sum( abs(f_free) ); % only focus on free parts
+
+        q(freeDOF) = q_free;
+
+    end
+
+    % Update
+    u = (q - q0) / dt; % Velocity
+
+    % New becomes Old
+    q0 = q; Q(:,c) = q0;
+    u0 = u;
+
+    Y_max(c) = min(q0);
+    
+
+end
+
+%% Plotting
+timeArray = (0:Nsteps-1) * dt;
+
+% Q1:
+figure(2); % Maximum vertical displacement
+plot(timeArray, Y_max, 'r-','LineWidth',1.5);
+xlabel('t (sec)');
+ylabel('y_{max} (meter)');
+xlim([0 1]);
+% title('Maximum vertical displacement of the beam');
+grid on
+
+% Euler Beam Theory:
+temp = min(Point, RodLength - Point);
+y_max = -P0*temp*(RodLength^2-temp^2)^1.5/(9*sqrt(3)*EI*RodLength);
+
+YY_max(i) = Y_max(c);
+yy_max(i) = y_max;
+
+end
+
+% Q2:
+figure(3); % P vs. y_max
+grid on;
+hold on;
+plot( PP0, YY_max, 'ro-','LineWidth',1.5); % plot Simulation results
+plot( PP0, yy_max, 'b*-','LineWidth',1.5); % plot Theory results
+legend('Simulation Results','Theory Results')
+xlabel('P (Newton)');
+ylabel('y_{max} (meter)');
+% title('Simulation and theory difference');
+
+
+
+
+%% Copyright M. Khalid Jawed (khalidjm@seas.ucla.edu)
+% You should use this code at your own risk. Copy and redistribution is not
+% permitted. Written permission is required.
+
+function dF = gradEb(xkm1, ykm1, xk, yk, xkp1, ykp1, curvature0, l_k, EI)
+
+%
+% This function returns the derivative of bending energy E_k^b with respect
+% to x_{k-1}, y_{k-1}, x_k, y_k, x_{k+1}, and y_{k+1}.
+%
+% curvature0 is the "discrete" natural curvature [dimensionless] at node (xk, yk).
+% l_k is the voronoi length of node (xk, yk).
+% EI is the bending stiffness.
+%
+
+node0 = [xkm1, ykm1, 0];
+node1 = [xk, yk, 0];
+node2 = [xkp1, ykp1, 0];
+%     m1e, 
+m2e = [0 0 1];
+%     m1f,
+m2f = [0 0 1];
+kappaBar = curvature0;
+
+%% Computation of gradient of the two curvatures
+gradKappa = zeros(6,1);
+
+ee = node1 - node0;
+ef = node2 - node1;
+
+norm_e = norm(ee);
+norm_f = norm(ef);
+
+te = ee / norm_e;
+tf = ef / norm_f;
+
+% Curvature binormal
+kb = 2.0 * cross(te, tf) / (1.0 + dot(te, tf));
+
+chi = 1.0 + dot(te, tf);
+tilde_t = (te + tf) / chi;
+tilde_d2 = (m2e + m2f) / chi;
+
+% Curvatures
+kappa1 = kb(3); % 0.5 * dot( kb, m2e + m2f); % CHECKED
+
+Dkappa1De = 1.0 / norm_e * (-kappa1 * tilde_t + cross(tf,tilde_d2));
+Dkappa1Df = 1.0 / norm_f * (-kappa1 * tilde_t - cross(te,tilde_d2));
+
+gradKappa(1:2, 1) = -Dkappa1De(1:2);
+gradKappa(3:4, 1) = Dkappa1De(1:2) - Dkappa1Df(1:2);
+gradKappa(5:6, 1) = Dkappa1Df(1:2);
+
+%% Gradient of Eb
+dkappa = kappa1 - kappaBar;
+dF = gradKappa * EI * dkappa / l_k;
+end
+
+function F = gradEs(xk, yk, xkp1, ykp1, l_k, EA)
+%
+% This function returns the derivative of stretching energy E_k^s with 
+% respect to x_{k-1}, y_{k-1}, x_k, and y_k.
+%
+F = zeros(4,1);
+
+F(1) = -(0.1e1 - sqrt((xkp1 - xk) ^ 2 + (ykp1 - yk) ^ 2) / l_k) * ((xkp1 - xk) ^ 2 + (ykp1 - yk) ^ 2) ^ (-0.1e1 / 0.2e1) / l_k * (-0.2e1 * xkp1 + 0.2e1 * xk);
+F(2) = -(0.1e1 - sqrt((xkp1 - xk) ^ 2 + (ykp1 - yk) ^ 2) / l_k) * ((xkp1 - xk) ^ 2 + (ykp1 - yk) ^ 2) ^ (-0.1e1 / 0.2e1) / l_k * (-0.2e1 * ykp1 + 0.2e1 * yk);
+F(3) = -(0.1e1 - sqrt((xkp1 - xk) ^ 2 + (ykp1 - yk) ^ 2) / l_k) * ((xkp1 - xk) ^ 2 + (ykp1 - yk) ^ 2) ^ (-0.1e1 / 0.2e1) / l_k * (0.2e1 * xkp1 - 0.2e1 * xk);
+F(4) = -(0.1e1 - sqrt((xkp1 - xk) ^ 2 + (ykp1 - yk) ^ 2) / l_k) * ((xkp1 - xk) ^ 2 + (ykp1 - yk) ^ 2) ^ (-0.1e1 / 0.2e1) / l_k * (0.2e1 * ykp1 - 0.2e1 * yk);
+
+F = 0.5 * EA * l_k * F;
+
+end
+
+
+%% Copyright M. Khalid Jawed (khalidjm@seas.ucla.edu)
+% You should use this code at your own risk. Copy and redistribution is not
+% permitted. Written permission is required.
+
+function dJ = hessEb(xkm1, ykm1, xk, yk, xkp1, ykp1, curvature0, l_k, EI)
+
+%
+% This function returns the derivative of bending energy E_k^b with respect
+% to x_{k-1}, y_{k-1}, x_k, y_k, x_{k+1}, and y_{k+1}.
+%
+% curvature0 is the "discrete" natural curvature [dimensionless] at node (xk, yk).
+% l_k is the voronoi length of node (xk, yk).
+% EI is the bending stiffness.
+%
+
+node0 = [xkm1, ykm1, 0];
+node1 = [xk, yk, 0];
+node2 = [xkp1, ykp1, 0];
+%     m1e, 
+m2e = [0 0 1];
+%     m1f,
+m2f = [0 0 1];
+kappaBar = curvature0;
+
+%% Computation of gradient of the two curvatures
+gradKappa = zeros(6,1);
+
+ee = node1 - node0;
+ef = node2 - node1;
+
+norm_e = norm(ee);
+norm_f = norm(ef);
+
+te = ee / norm_e;
+tf = ef / norm_f;
+
+% Curvature binormal
+kb = 2.0 * cross(te, tf) / (1.0 + dot(te, tf));
+
+chi = 1.0 + dot(te, tf);
+tilde_t = (te + tf) / chi;
+tilde_d2 = (m2e + m2f) / chi;
+
+% Curvatures
+kappa1 = kb(3); % 0.5 * dot( kb, m2e + m2f); % CHECKED
+
+Dkappa1De = 1.0 / norm_e * (-kappa1 * tilde_t + cross(tf,tilde_d2));
+Dkappa1Df = 1.0 / norm_f * (-kappa1 * tilde_t - cross(te,tilde_d2));
+
+gradKappa(1:2, 1) = -Dkappa1De(1:2);
+gradKappa(3:4, 1) = Dkappa1De(1:2) - Dkappa1Df(1:2);
+gradKappa(5:6, 1) = Dkappa1Df(1:2);
+
+%% Computation of hessian of the two curvatures
+DDkappa1 = zeros(6, 6);
+% DDkappa2 = zeros(11, 11);
+
+norm2_e = norm_e^2;
+norm2_f = norm_f^2;
+
+tt_o_tt = tilde_t' * tilde_t; % must be 3x3. tilde_t is 1x3
+tmp = cross(tf, tilde_d2);
+tf_c_d2t_o_tt = tmp' * tilde_t; % must be 3x3
+tt_o_tf_c_d2t = tf_c_d2t_o_tt'; % must be 3x3
+kb_o_d2e = kb' * m2e; % must be 3x3
+d2e_o_kb = kb_o_d2e'; % must be 3x3
+
+Id3 = eye(3);
+D2kappa1De2 ...
+    = 1.0 / norm2_e * (2 * kappa1 * tt_o_tt - tf_c_d2t_o_tt - tt_o_tf_c_d2t) ...
+    - kappa1 / (chi * norm2_e) * (Id3 - te'*te) ...
+    + 1.0 / (4.0 * norm2_e) * (kb_o_d2e + d2e_o_kb);
+
+tmp = cross(te, tilde_d2);
+te_c_d2t_o_tt = tmp' * tilde_t;
+tt_o_te_c_d2t = te_c_d2t_o_tt';
+kb_o_d2f = kb' * m2f;
+d2f_o_kb = kb_o_d2f';
+
+D2kappa1Df2 ...
+    = 1.0 / norm2_f * (2 * kappa1 * tt_o_tt + te_c_d2t_o_tt + tt_o_te_c_d2t) ...
+    - kappa1 / (chi * norm2_f) * (Id3 - tf'*tf) ...
+    + 1.0 / (4.0 * norm2_f) * (kb_o_d2f + d2f_o_kb);
+
+D2kappa1DeDf ...
+    = -kappa1/(chi * norm_e * norm_f) * (Id3 + te'*tf) ...
+    + 1.0 / (norm_e*norm_f) * (2 * kappa1 * tt_o_tt - tf_c_d2t_o_tt + ...
+    tt_o_te_c_d2t - crossMat(tilde_d2));
+D2kappa1DfDe = D2kappa1DeDf';
+
+% Curvature terms
+DDkappa1(1:2, 1:2)  =   D2kappa1De2(1:2, 1:2);
+DDkappa1(1:2, 3:4)  = - D2kappa1De2(1:2, 1:2) + D2kappa1DeDf(1:2, 1:2);
+DDkappa1(1:2, 5:6) =               - D2kappa1DeDf(1:2, 1:2);
+DDkappa1(3:4, 1:2)  = - D2kappa1De2(1:2, 1:2)                + D2kappa1DfDe(1:2, 1:2);
+DDkappa1(3:4, 3:4)  =   D2kappa1De2(1:2, 1:2) - D2kappa1DeDf(1:2, 1:2) - ...
+    D2kappa1DfDe(1:2, 1:2) + D2kappa1Df2(1:2, 1:2);
+DDkappa1(3:4, 5:6) =                 D2kappa1DeDf(1:2, 1:2)                - D2kappa1Df2(1:2, 1:2);
+DDkappa1(5:6, 1:2)  =                              - D2kappa1DfDe(1:2, 1:2);
+DDkappa1(5:6, 3:4)  =                                D2kappa1DfDe(1:2, 1:2) - D2kappa1Df2(1:2, 1:2);
+DDkappa1(5:6, 5:6) =                                               D2kappa1Df2(1:2, 1:2);
+
+%% Hessian of Eb
+dkappa = kappa1 - kappaBar;
+dJ = 1.0 / l_k * EI * gradKappa * transpose(gradKappa);
+temp = 1.0 / l_k * dkappa * EI;
+dJ = dJ + temp * DDkappa1;
+
+end
+
+function A = crossMat(a)
+A = [0, -a(3), a(2); ...
+    a(3), 0, -a(1); ...
+    -a(2), a(1), 0];
+end
+
+function J = hessEs(xk, yk, xkp1, ykp1, l_k, EA)
+%
+% This function returns the 4x4 hessian of the stretching energy E_k^s with
+% respect to x_k, y_k, x_{k+1}, and y_{k+1}.
+%
+J11 = (1 / ((xkp1 - xk) ^ 2 + (ykp1 - yk) ^ 2) / l_k ^ 2 * (-2 * xkp1 + 2 * xk) ^ 2) / 0.2e1 + (0.1e1 - sqrt(((xkp1 - xk) ^ 2 + (ykp1 - yk) ^ 2)) / l_k) * (((xkp1 - xk) ^ 2 + (ykp1 - yk) ^ 2) ^ (-0.3e1 / 0.2e1)) / l_k * ((-2 * xkp1 + 2 * xk) ^ 2) / 0.2e1 - 0.2e1 * (0.1e1 - sqrt(((xkp1 - xk) ^ 2 + (ykp1 - yk) ^ 2)) / l_k) * (((xkp1 - xk) ^ 2 + (ykp1 - yk) ^ 2) ^ (-0.1e1 / 0.2e1)) / l_k;
+J12 = (1 / ((xkp1 - xk) ^ 2 + (ykp1 - yk) ^ 2) / l_k ^ 2 * (-2 * ykp1 + 2 * yk) * (-2 * xkp1 + 2 * xk)) / 0.2e1 + (0.1e1 - sqrt(((xkp1 - xk) ^ 2 + (ykp1 - yk) ^ 2)) / l_k) * (((xkp1 - xk) ^ 2 + (ykp1 - yk) ^ 2) ^ (-0.3e1 / 0.2e1)) / l_k * (-2 * xkp1 + 2 * xk) * (-2 * ykp1 + 2 * yk) / 0.2e1;
+J13 = (1 / ((xkp1 - xk) ^ 2 + (ykp1 - yk) ^ 2) / l_k ^ 2 * (2 * xkp1 - 2 * xk) * (-2 * xkp1 + 2 * xk)) / 0.2e1 + (0.1e1 - sqrt(((xkp1 - xk) ^ 2 + (ykp1 - yk) ^ 2)) / l_k) * (((xkp1 - xk) ^ 2 + (ykp1 - yk) ^ 2) ^ (-0.3e1 / 0.2e1)) / l_k * (-2 * xkp1 + 2 * xk) * (2 * xkp1 - 2 * xk) / 0.2e1 + 0.2e1 * (0.1e1 - sqrt(((xkp1 - xk) ^ 2 + (ykp1 - yk) ^ 2)) / l_k) * (((xkp1 - xk) ^ 2 + (ykp1 - yk) ^ 2) ^ (-0.1e1 / 0.2e1)) / l_k;
+J14 = (1 / ((xkp1 - xk) ^ 2 + (ykp1 - yk) ^ 2) / l_k ^ 2 * (2 * ykp1 - 2 * yk) * (-2 * xkp1 + 2 * xk)) / 0.2e1 + (0.1e1 - sqrt(((xkp1 - xk) ^ 2 + (ykp1 - yk) ^ 2)) / l_k) * (((xkp1 - xk) ^ 2 + (ykp1 - yk) ^ 2) ^ (-0.3e1 / 0.2e1)) / l_k * (-2 * xkp1 + 2 * xk) * (2 * ykp1 - 2 * yk) / 0.2e1;
+J22 = (1 / ((xkp1 - xk) ^ 2 + (ykp1 - yk) ^ 2) / l_k ^ 2 * (-2 * ykp1 + 2 * yk) ^ 2) / 0.2e1 + (0.1e1 - sqrt(((xkp1 - xk) ^ 2 + (ykp1 - yk) ^ 2)) / l_k) * (((xkp1 - xk) ^ 2 + (ykp1 - yk) ^ 2) ^ (-0.3e1 / 0.2e1)) / l_k * ((-2 * ykp1 + 2 * yk) ^ 2) / 0.2e1 - 0.2e1 * (0.1e1 - sqrt(((xkp1 - xk) ^ 2 + (ykp1 - yk) ^ 2)) / l_k) * (((xkp1 - xk) ^ 2 + (ykp1 - yk) ^ 2) ^ (-0.1e1 / 0.2e1)) / l_k;
+J23 = (1 / ((xkp1 - xk) ^ 2 + (ykp1 - yk) ^ 2) / l_k ^ 2 * (2 * xkp1 - 2 * xk) * (-2 * ykp1 + 2 * yk)) / 0.2e1 + (0.1e1 - sqrt(((xkp1 - xk) ^ 2 + (ykp1 - yk) ^ 2)) / l_k) * (((xkp1 - xk) ^ 2 + (ykp1 - yk) ^ 2) ^ (-0.3e1 / 0.2e1)) / l_k * (-2 * ykp1 + 2 * yk) * (2 * xkp1 - 2 * xk) / 0.2e1;
+J24 = (1 / ((xkp1 - xk) ^ 2 + (ykp1 - yk) ^ 2) / l_k ^ 2 * (2 * ykp1 - 2 * yk) * (-2 * ykp1 + 2 * yk)) / 0.2e1 + (0.1e1 - sqrt(((xkp1 - xk) ^ 2 + (ykp1 - yk) ^ 2)) / l_k) * (((xkp1 - xk) ^ 2 + (ykp1 - yk) ^ 2) ^ (-0.3e1 / 0.2e1)) / l_k * (-2 * ykp1 + 2 * yk) * (2 * ykp1 - 2 * yk) / 0.2e1 + 0.2e1 * (0.1e1 - sqrt(((xkp1 - xk) ^ 2 + (ykp1 - yk) ^ 2)) / l_k) * (((xkp1 - xk) ^ 2 + (ykp1 - yk) ^ 2) ^ (-0.1e1 / 0.2e1)) / l_k;
+J33 = (1 / ((xkp1 - xk) ^ 2 + (ykp1 - yk) ^ 2) / l_k ^ 2 * (2 * xkp1 - 2 * xk) ^ 2) / 0.2e1 + (0.1e1 - sqrt(((xkp1 - xk) ^ 2 + (ykp1 - yk) ^ 2)) / l_k) * (((xkp1 - xk) ^ 2 + (ykp1 - yk) ^ 2) ^ (-0.3e1 / 0.2e1)) / l_k * ((2 * xkp1 - 2 * xk) ^ 2) / 0.2e1 - 0.2e1 * (0.1e1 - sqrt(((xkp1 - xk) ^ 2 + (ykp1 - yk) ^ 2)) / l_k) * (((xkp1 - xk) ^ 2 + (ykp1 - yk) ^ 2) ^ (-0.1e1 / 0.2e1)) / l_k;
+J34 = (1 / ((xkp1 - xk) ^ 2 + (ykp1 - yk) ^ 2) / l_k ^ 2 * (2 * ykp1 - 2 * yk) * (2 * xkp1 - 2 * xk)) / 0.2e1 + (0.1e1 - sqrt(((xkp1 - xk) ^ 2 + (ykp1 - yk) ^ 2)) / l_k) * (((xkp1 - xk) ^ 2 + (ykp1 - yk) ^ 2) ^ (-0.3e1 / 0.2e1)) / l_k * (2 * xkp1 - 2 * xk) * (2 * ykp1 - 2 * yk) / 0.2e1;
+J44 = (1 / ((xkp1 - xk) ^ 2 + (ykp1 - yk) ^ 2) / l_k ^ 2 * (2 * ykp1 - 2 * yk) ^ 2) / 0.2e1 + (0.1e1 - sqrt(((xkp1 - xk) ^ 2 + (ykp1 - yk) ^ 2)) / l_k) * (((xkp1 - xk) ^ 2 + (ykp1 - yk) ^ 2) ^ (-0.3e1 / 0.2e1)) / l_k * ((2 * ykp1 - 2 * yk) ^ 2) / 0.2e1 - 0.2e1 * (0.1e1 - sqrt(((xkp1 - xk) ^ 2 + (ykp1 - yk) ^ 2)) / l_k) * (((xkp1 - xk) ^ 2 + (ykp1 - yk) ^ 2) ^ (-0.1e1 / 0.2e1)) / l_k;
+
+J = [J11 J12 J13 J14;
+     J12 J22 J23 J24;
+     J13 J23 J33 J34;
+     J14 J24 J34 J44];
+
+J = 0.5 * EA * l_k * J;
+
+end
 
