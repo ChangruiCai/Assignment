5a5da69e3e7a5bf09ca332ba6c6cd38e09a10d3f
ChangruiCai <changrui05@ucla.edu>
2024-12-10 23:14:01 -0800

    Delete HW3/Option1-problem1 directory

--- HW3/Option1-problem1/Readme.m
+++
@@ -1,3 +0,0 @@
-Run the Problem1.py to get the reasult.
-You can revise the epochs and learning rate by revising the parameter in linear_fit(10000, 0.001).
-The first one is epochs number, and the second is learning rate.
